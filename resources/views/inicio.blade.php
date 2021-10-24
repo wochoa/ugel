f536922be4bf946540567e29680a4d37cbc7e421
unknown <woalvarado1@gmail.com>
2021-10-23 19:49:02 -0500

    quince

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -325,7 +325,7 @@ $dnsserver=$tematm["dnsserver"];
             </a>
             {{-- <div class="dropdown-divider"></div> --}}
               @else
-              <a href="/anuncios" class="btn btn-info btn-block mb-3 titulonot_2"><i class="fas fa-address-card"></i> Ver todo los anuncios publicados</a>
+              <a href="{{ $sec5->enlace }}" class="btn btn-{{ $sec5->color }} btn-block mb-3 titulonot_2"><i class="fas fa-address-card"></i> {{ $sec5->texto_enlace }}</a>
             @endif
               
             @endforeach
